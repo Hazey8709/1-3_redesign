3890ac960aafa7b709158623dfc2cb74d34cbf7b
Hazey8709 <jfortin802@gmail.com>
2022-12-01 18:09:36 -0500

    updated files installed react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>running! Good Luck$</h1>
+            <img src={logo} className='App-logo' alt='logo' />
+        </div>
+    );
+}
+
+export default App;
